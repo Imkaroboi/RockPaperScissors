a04c9d7128ccc08c84b7440d0b7dc8aa9d483f7d
Imkaroboi <sarveshshirashyad@gmail.com>
2024-05-06 14:52:05 -0700

    worked on js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function rock() {
-
+    choice()
 };
 function paper() {
 
@@ -7,3 +7,11 @@ function paper() {
 function scissors() {
 
 };
+function choice() {
+let computerChoice = Math.random();
+if (computerChoice < 0.3333333) {
+    console.log("rock");
+} else if (computerChoice > 0.33333333 && computerChoice < 0.66666666) {
+    console.log("paper")
+} else (console.log("scissors"))
+}
