4c167992b00c20a4a42be797415bbb68c1988df7
Imkaroboi <sarveshshirashyad@gmail.com>
2024-05-06 22:48:25 -0700

    js work

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function rock() {
-    choice()
+    let playerChoice = "rock"
+    if (playerChoice === computer) {
+        alert("hi")
+    }
 };
 function paper() {
 
@@ -7,11 +10,12 @@ function paper() {
 function scissors() {
 
 };
+let computer;
 function choice() {
 let computerChoice = Math.random();
 if (computerChoice < 0.3333333) {
-    console.log("rock");
+    let computer = "rock"
 } else if (computerChoice > 0.33333333 && computerChoice < 0.66666666) {
-    console.log("paper")
-} else (console.log("scissors"))
+    let computer = "paper"
+} else {let computer = "scissors"}
 }
