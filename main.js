3d60ede9ee165e65cfb4a22253bd0ee9aae5b1ed
Imkaroboi <sarveshshirashyad@gmail.com>
2024-05-12 17:40:15 -0700

    final

--- main.js
+++ main.js
@@ -5,22 +5,55 @@ function rock() {
     let computerChoice = Math.random();
     if (playerChoice >= computerChoice) {
         alert("tie")
+        document.getElementById("text").innerHTML = "Tie"
     } else if (computerChoice > 0.333333 && computerChoice < 0.666666) {
-        alert("You Win")
+        alert("You Win, rock beats scissors!")
          playerScore++
         document.getElementById("yourScore").innerHTML = `Your Score - ${playerScore}`
+        document.getElementById("text").innerHTML = "You Win"
     } else {
-        document.getElementById("text").innerHTML = "Computer played paper, You lose!"
         computerScore++
         document.getElementById("compScore").innerHTML = `Computer Score - ${computerScore}`
+        alert("You Lose, Rock loses to Paper!")
+        document.getElementById("text").innerHTML = "You Lose";
     }
 
 };
 function paper() {
     let playerChoice = 0.666666
+    let computerChoice = Math.random();
+    if (playerChoice >= computerChoice && computerChoice > 0.333333){
+        alert("tie")
+        document.getElementById("text").innerHTML = "Tie";
+    } else if (computerChoice <= 0.333333) {
+        alert("Computer played rock, you win!");
+        playerScore++
+        document.getElementById("yourScore").innerHTML = `Your Score - ${playerScore}`;
+        document.getElementById("text").innerHTML = "You Win";
+    } else {
+        alert("Computer played scissors, you lose!")
+        computerScore++
+        document.getElementById("compScore").innerHTML = `Computer Score - ${computerScore}`
+        document.getElementById("text").innerHTML = "You Lose";
+    }
 };
 function  scissors() {
     let playerChoice = 0.999999
+    let computerChoice = Math.random();
+    if (computerChoice <= 0.333333) {
+        alert("You Lose, computer played rock!")
+        computerScore++
+        document.getElementById("compScore").innerHTML = `Computer Score - ${computerScore}`
+        document.getElementById("text").innerHTML = "You Lose";
+    } else if (computerChoice > 0.333333 && computerChoice <= 0.666666) {
+        alert("You Win, computer played Paper!");
+        playerScore++
+        document.getElementById("yourScore").innerHTML = `Your Score - ${playerScore}`;
+        document.getElementById("text").innerHTML = "You Win";
+    } else {
+        alert("tie");
+        document.getElementById("text").innerHTML = "Tie";
+    }
 };
 let computer;
 function choice() {
